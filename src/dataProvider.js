07c0f82a5035ac3715c8eb8fa0786d48dbac7c14
mathiastj <2278040+mathiastj@users.noreply.github.com>
2022-09-13 21:47:47 +0200

    Hackish data provider wip

--- src/dataProvider.js
+++ src/dataProvider.js
@@ -1,23 +1,26 @@
-import { supabaseDataProvider } from 'ra-supabase'
+import { supabaseDataProvider } from './fixed-data-provider'
 import { supabase } from './supabase'
 
 const resources = {
   categories: ['id', 'name', 'created_at', 'updated_at'],
-  recipes: [
-    'id',
-    'title',
-    'ingredients',
-    'directions',
-    'rating',
-    'created_at',
-    'updated_at',
-    'servings',
-    'category_id',
-    'duration',
-    'duration_free',
-    'source',
-    'season',
-  ],
+  recipes: {
+    fullTextSearchFields: ['title', 'ingredients', 'season'],
+    fields: [
+      'id',
+      'title',
+      'ingredients',
+      'directions',
+      'rating',
+      'created_at',
+      'updated_at',
+      'servings',
+      'category_id',
+      'duration',
+      'duration_free',
+      'source',
+      'season',
+    ],
+  },
 }
 
 export const dataProvider = supabaseDataProvider(supabase, resources)
